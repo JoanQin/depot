c968fe7f0c518df972d4aed33cad77a92f3b2671
JoanQin <qin.42@osu.edu>
2011-05-20 15:28:54 -0400

    new changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private 
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+    
+  public
+    def index_count
+      if session[:counter].nil?
+        session[:counter] = 1
+        return session[:counter]
+      else
+        session[:counter] = session[:counter] + 1
+        return session[:counter]
+      end
+    end
+    
+    def reset_count
+      session[:counter] = 0
+      return session[:counter]
+    end
 end
